599b69f357a4fedf2e508b080a491df358540cb0
Brian Yingling <brian@jet.com>
2018-05-13 18:06:05 -0400

    added basic logger wrapper

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ require('dotenv').load();
 var express = require('express');
 var bodyParser = require('body-parser');
 var winston = require('winston');
+var logger = require('./lib/logger');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 var API_URL = process.env.API_URL;
 
-winston.add(winston.transports.File, {filename: 'incoming.log'});
+logger.add('file', {filename: 'incoming.log'});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('*', function(req, res) {
-    winston.log('info', req.body);
-    // console.log(req.body);
+    logger.info('info', req.body);
 });
 
 app.listen(PORT, (req, res) => {
