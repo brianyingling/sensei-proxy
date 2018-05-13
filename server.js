1de37f69d8a9727b7661eed1ccbc6d64ee87ed00
Brian Yingling <brian@jet.com>
2018-05-13 16:28:40 -0400

    added gitignore

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').load();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+var API_URL = process.env.API_URL;
+
+console.log("URL:", API_URL);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('*', function(req, res) {
+    console.log(req.body);
+});
+
+app.listen(PORT, (req, res) => {
+    console.log('Sensei Proxy');
+    console.log('Listening on port ' + PORT)
+});
