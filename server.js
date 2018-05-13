32944e79b871176ee05796f2878b1e7127bfcb56
Brian Yingling <brian@jet.com>
2018-05-13 17:20:20 -0400

    main file is now server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').load();
 var express = require('express');
 var bodyParser = require('body-parser');
+var winston = require('winston');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
@@ -10,7 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('*', function(req, res) {
-    console.log(req.body);
+    winston.log('info', req.body);
+    // console.log(req.body);
 });
 
 app.listen(PORT, (req, res) => {
